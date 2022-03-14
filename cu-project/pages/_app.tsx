8fcb6dbaedde8e5fb3c472ef7706ebbd77acf916
choigeon <ck731@naver.com>
2022-03-14 19:39:38 +0900

    feat/#9/project setting

---
+++ cu-project/pages/_app.tsx
@@ -0,0 +1,38 @@
+import {
+  ApolloClient,
+  ApolloLink,
+  ApolloProvider,
+  InMemoryCache,
+} from "@apollo/client";
+import { AppProps } from "next/app";
+import { createUploadLink } from "apollo-upload-client";
+import "../styles/globals.css";
+import React from "react";
+import { Global } from "@emotion/react";
+import { globalStyles } from "../src/commons/styles/globalStyles";
+import Layout from "../src/commons/layout";
+
+function MyApp({ Component, pageProps }: AppProps) {
+  const uploadLink = createUploadLink({
+    uri: "APIURL",
+    credentials: "include",
+  });
+
+  const client = new ApolloClient({
+    // link: ApolloLink.from([errorLink, uploadLink]),
+    link: ApolloLink.from([uploadLink]),
+    cache: new InMemoryCache(),
+    connectToDevTools: true,
+  });
+
+  return (
+    <ApolloProvider client={client}>
+      <Global styles={globalStyles} />
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </ApolloProvider>
+  );
+}
+
+export default MyApp;
