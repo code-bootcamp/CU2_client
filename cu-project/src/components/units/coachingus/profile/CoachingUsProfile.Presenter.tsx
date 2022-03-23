4f59ebfcbb02d74a29d73cc8a1430b3ce2a3a5df
kth7024 <guinness5167@gmail.com>
2022-03-23 18:33:02 +0900

    feat/#85/coachdetail coachdetail setting clear

--- cu-project/src/components/units/coachingus/profile/CoachingUsProfile.Presenter.tsx
+++ cu-project/src/components/units/coachingus/profile/CoachingUsProfile.Presenter.tsx
@@ -1,7 +1,6 @@
 import { ICoachingUsProfileUIProps } from "../../../../commons/types/types";
 import * as S from "./CoachingUsProfile.Style";
 
-export default function CoachingUsProfileUI (props: ICoachingUsProfileUIProps) {
-    
-    return <S.Wrapper>CodingUs Coach Profile 페이지입니다.</S.Wrapper>;
+export default function CoachingUsProfileUI(props: ICoachingUsProfileUIProps) {
+  return <S.Wrapper>CodingUs Coach Profilea 페aa이지a입니다.</S.Wrapper>;
 }
