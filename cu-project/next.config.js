c3a16ae60a17f0ba24e8c162deec38a1bc1aadc8
choigeon <ck731@naver.com>
2022-03-23 23:07:18 +0900

    feat/#103/AddIgnoreBuildErrorOption fix errors

--- cu-project/next.config.js
+++ cu-project/next.config.js
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  typescript:{
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  ignoreBuildErrors: true
 }
 
 module.exports = nextConfig
