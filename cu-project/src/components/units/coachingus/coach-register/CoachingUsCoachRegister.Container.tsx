d7f5894b9b1efb27210da17995d2d1d3d09416f9
kth7024 <guinness5167@gmail.com>
2022-03-28 16:44:22 +0900

    feat/#159/mentoRegister mentoRegister finish

--- cu-project/src/components/units/coachingus/coach-register/CoachingUsCoachRegister.Container.tsx
+++ cu-project/src/components/units/coachingus/coach-register/CoachingUsCoachRegister.Container.tsx
@@ -1,5 +1,59 @@
+import { useCallback, useState } from "react";
 import CoachingUsCoachRegisterUI from "./CoachingUsCoachRegister.Presenter";
 
 export default function CoachingUsCoachRegisterPage() {
-  return <CoachingUsCoachRegisterUI />;
+  const [text, setText] = useState("");
+  const [tags, setTags] = useState([]);
+  const [corType, setCorType] = useState("");
+
+  const onChangeTags = (e) => {
+    if (e.isComposing || e.keyCode === 229) {
+      return;
+    }
+
+    if (e.key === "Backspace" && e.target.value === "") {
+      console.log(e.target.id);
+      return setTags(tags.filter((_, i) => i + 1 !== tags.length));
+    } else {
+      if (tags.length > 1) {
+        e.target.value = "";
+        return alert("최대 네 개까지 입력 가능합니다.");
+      } else {
+        if (e.key === "Enter") {
+          if (e.target.value === "") {
+            setTags([...tags]);
+            e.target.value = "";
+          } else {
+            setTags([...tags, e.target.value]);
+            e.target.value = "";
+          }
+        }
+      }
+    }
+  };
+
+  const onChangeText = (e) => {
+    setText(e.target.value);
+  };
+
+  const onClickDeleteTags = (e) => {
+    setTags(tags.filter((_, i) => Number(e.target.id) !== i));
+  };
+
+  const onClickCorType = (e) => {
+    setCorType(e.target.value);
+  };
+
+  return (
+    <CoachingUsCoachRegisterUI
+      onChangeTags={onChangeTags}
+      onClickDeleteTags={onClickDeleteTags}
+      onChangeText={onChangeText}
+      tags={tags}
+      setTags={setTags}
+      text={text}
+      onClickCorType={onClickCorType}
+      corType={corType}
+    />
+  );
 }
--- cu-project/src/components/units/coachingus/coach-register/CoachingUsCoachRegister.Style.ts
+++ cu-project/src/components/units/coachingus/coach-register/CoachingUsCoachRegister.Style.ts
@@ -5,17 +5,145 @@ export const Wrapper = styled.div`
   min-height: calc(100vh - 30px);
   box-sizing: border-box;
   margin: 0;
-  background-image: url("/hero-image-bg.webp");
-  background-color: transparent;
-  background-size: 100vw;
-
   display: flex;
   align-items: center;
   justify-content: center;
+  background: linear-gradient(to bottom right, #d7455d, #6917d2);
+  /* background: linear-gradient(to bottom right, #413e98, #ab4db7); */
 `;
 
 export const CoachingRegisterContainer = styled.div`
-  width: 500px;
-  height: 600px;
+  width: 900px;
+  height: 500px;
   background-color: white;
+  display: flex;
+  box-shadow: 0 14px 18px 0 rgba(0, 0, 0, 0.2),
+    0 16px 20px 0 rgba(0, 0, 0, 0.19);
+  animation: fadeIn 2s ease-in-out;
+  @keyframes fadeIn {
+    0% {
+      opacity: 0;
+      transform: translateY(60px);
+    }
+    100% {
+      opacity: 1;
+      transform: translateY(0);
+    }
+  }
+`;
+export const RegisterContainerLeft = styled.div`
+  display: flex;
+  justify-content: center;
+  width: 450px;
+
+  background-color: white;
+  overflow: scroll;
+
+  ::-webkit-scrollbar {
+    display: none;
+  }
+`;
+export const RegisterContainerRight = styled.div`
+  width: 450px;
+  height: 100%;
+  background-image: url("/registerImg.png");
+  background-size: 100%;
+`;
+
+// register
+
+export const RegisterForm = styled.div`
+  min-width: 400px;
+  height: max-content;
+  padding: 40px 20px;
+  display: flex;
+  flex-direction: column;
+`;
+export const FormTitle = styled.h1`
+  all: unset;
+  font-size: 24px;
+  font-weight: 700;
+  color: #bdbdbd;
+`;
+export const FormSubTitle = styled.p``;
+
+export const FormInput = styled.input`
+  all: unset;
+  width: 70%;
+  line-height: 20px;
+  padding: 5px;
+  transition: border 1s ease-in-out;
+
+  border-bottom: ${({ isFull }) =>
+    isFull ? "1px solid #bdbdbd" : "1px solid white"};
+
+  :focus {
+    auto: true;
+    border-bottom: 1px solid #bdbdbd;
+  }
+`;
+
+export const CorGroup = styled.div`
+  display: flex;
+`;
+export const CorGroupButton = styled.button`
+  all: unset;
+  background-color: ${({ isCurrent }) => (isCurrent ? "#ea345a" : "white")};
+  color: ${({ isCurrent }) => (isCurrent ? "white" : "#ea345a")};
+
+  border: 1px solid #ea345a;
+  border-radius: 15px;
+  padding: 5px 10px;
+
+  :hover {
+    background-color: #ea345a;
+    color: white;
+    border: 1px solid #ea345a;
+  }
+`;
+export const RegisterButton = styled.button`
+  all: unset;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: white;
+  color: #ea345a;
+  border: 1px solid #ea345a;
+  border-radius: 15px;
+  padding: 5px 10px;
+  width: 100px;
+  :hover {
+    background-color: #ea345a;
+    color: white;
+    border: 1px solid #ea345a;
+  }
+`;
+
+export const TagBox = styled.div`
+  display: flex;
+  align-items: center;
+  flex-wrap: wrap;
+  padding: 5px 0;
+`;
+
+export const TagInput = styled.input`
+  display: ${({ isFull }) => (isFull ? "none" : "flex")};
+  height: 35px;
+  padding-left: 5px;
+  margin-left: 5px;
+  border: none;
+  :focus {
+    outline: none;
+    border: none;
+  }
+`;
+export const TagItem = styled.div`
+  margin: 0 5px 5px 5px;
+  color: white;
+  font-weight: normal;
+  background: #ea345a;
+  padding: 5px 15px;
+  border-radius: 12px;
+
+  cursor: pointer;
 `;
