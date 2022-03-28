6b0aacda17070296e252a6afc75e090913f90e77
choigeon <ck731@naver.com>
2022-03-28 18:40:19 +0900

    feat/#161/AdCommentEdit clear

--- cu-project/pages/componentTestPage/index.tsx
+++ cu-project/pages/componentTestPage/index.tsx
@@ -3,7 +3,7 @@ import QnADetailCard from "../../src/components/units/codingus/card/qnaDetailCar
 export default function ComponentTestPage() {
   return (
     <div style={{marginTop:"200px"}}>
-      <QnADetailCard title={"자바스트립트가 뭔가요?"} writer="작성자" contents="자바스크립트가 궁금해요!" createdAt="2022-02-07T14:42:53.532Z" isGood={true}/>
+      {/* <QnADetailCard title={"자바스트립트가 뭔가요?"} writer="작성자" contents="자바스크립트가 궁금해요!" createdAt="2022-02-07T14:42:53.532Z" isGood={true}/> */}
     </div>
   );
 }
